5f31f87245d5f28048ee66457ed86adc2f23e575
SquidGodDev <tako48188@gmail.com>
2022-12-28 11:01:18 -0800

    Refactored movement code to seperate state change and collisions more

--- scripts/game/player.lua
+++ scripts/game/player.lua
@@ -30,10 +30,16 @@ function Player:init(x, y, gameManager)
     self.drag = 0.1
     self.acceleration = 0.5
 
+    self.touchedGround = true
+    self.touchedCeiling = false
+    self.touchedWall = false
+
     -- Climb
     self.climbVelocity = 3
     self.climbMagnetRange = 8
-
+    self.touchingClimableTile = false
+    self.standingOnClimableTile = false
+    self.climbTileX = self.x
 
     self:setCollideRect(8, 4, 18, 32)
     self:setGroups(COLLISION_GROUPS.player)
@@ -58,22 +64,18 @@ function Player:update()
 
     if self.currentState == "idle" then
         if pd.buttonIsPressed(pd.kButtonA)then
-            self.yVelocity = self.jumpVelocity
-            self:changeState("jumpAscent")
+            self:changeToJumpState()
         elseif pd.buttonIsPressed(pd.kButtonLeft) then
-            self.xVelocity = -self.startVelocity
-            self.globalFlip = 1
-            self:changeState("run")
+            self:changeToRunState("left")
         elseif pd.buttonIsPressed(pd.kButtonRight) then
-            self.xVelocity = self.startVelocity
-            self.globalFlip = 0
-            self:changeState("run")
+            self:changeToRunState("right")
         end
+        self:checkIfClimbing()
         self:applyFriction()
+        self:applyGravity()
     elseif self.currentState == "run" then
         if pd.buttonIsPressed(pd.kButtonA) then
-            self.yVelocity = self.jumpVelocity
-            self:changeState("jumpAscent")
+            self:changeToJumpState()
         elseif pd.buttonIsPressed(pd.kButtonLeft) then
             self:accelerateLeft()
         elseif pd.buttonIsPressed(pd.kButtonRight) then
@@ -81,28 +83,30 @@ function Player:update()
         else
             self:changeState("idle")
         end
+        self:checkIfClimbing()
+        self:applyGravity()
     elseif self.currentState == "jumpAscent" then
         self:handleJumpPhysics()
         if self.yVelocity >= 0 then
             self:changeState("jumpDescent")
         end
+        self:checkIfClimbing()
+        self:applyGravity()
     elseif self.currentState == "jumpDescent" then
         self:handleJumpPhysics()
-        if math.abs(self.yVelocity) < 0.5 then
+        if math.abs(self.yVelocity) <= 0 then
             if pd.buttonIsPressed(pd.kButtonLeft) then
-                self.xVelocity = -self.startVelocity
-                self.globalFlip = 1
-                self:changeState("run")
+                self:changeToRunState("left")
             elseif pd.buttonIsPressed(pd.kButtonRight) then
-                self.xVelocity = self.startVelocity
-                self.globalFlip = 0
-                self:changeState("run")
+                self:changeToRunState("right")
             else
                 self:changeState("idle")
             end
         end
+        self:checkIfClimbing()
+        self:applyGravity()
     elseif self.currentState == "climb" then
-        if pd.buttonIsPressed(pd.kButtonLeft) or pd.buttonIsPressed(pd.kButtonRight) then
+        if pd.buttonIsPressed(pd.kButtonLeft) or pd.buttonIsPressed(pd.kButtonRight) or not self.touchingClimableTile then
             self.yVelocity = 0
             self:changeState("idle")
             self._enabled = true
@@ -118,51 +122,7 @@ function Player:update()
         end
     end
 
-    local originalY = self.y
-    local _, _, collisions, length = self:moveWithCollisions(self.x + self.xVelocity, self.y + self.yVelocity)
-    local touchedGround = false
-    local touchedClimableTile = false
-    for i=1,length do
-        local collision = collisions[i]
-        local collisionType = collision.type
-        if collisionType == gfx.sprite.kCollisionTypeOverlap then
-            local collisionTag = collision.other:getTag()
-            if collisionTag == TAGS.CLIMABLE then
-                touchedClimableTile = true
-                if self.currentState ~= "climb" then
-                    if collision.normal.y == -1 and not collision.overlaps then
-                        if pd.buttonIsPressed(pd.kButtonDown) then
-                            self:changeToClimbState(collision)
-                        else
-                            self.yVelocity = 0
-                            touchedGround = true
-                            self:moveTo(self.x, originalY)
-                        end
-                    elseif pd.buttonIsPressed(pd.kButtonUp) then
-                        self:changeToClimbState(collision)
-                    end
-                end
-            end
-        else
-            if collision.normal.y == -1 then
-                touchedGround = true
-            elseif collision.normal.y == 1 then
-                self.yVelocity = 0
-            end
-            if collision.normal.x == -1 or collision.normal.x == 1 then
-                self.xVelocity = 0
-            end
-        end
-    end
-    if touchedGround then
-        self.yVelocity = 0
-    else
-        self:applyGravity()
-    end
-    if not touchedClimableTile and self.currentState == "climb" then
-        self:changeState("idle")
-        self.yVelocity = 0
-    end
+    self:handleMovementAndCollisions()
 
     if self.xVelocity < 0 then
         self.globalFlip = 1
@@ -181,22 +141,92 @@ function Player:update()
 	end
 end
 
+function Player:handleMovementAndCollisions()
+    local originalY = self.y
+    local _, _, collisions, length = self:moveWithCollisions(self.x + self.xVelocity, self.y + self.yVelocity)
+    self.touchedGround = false
+    self.touchedCeiling = false
+    self.touchedWall = false
+    self.touchingClimableTile = false
+    self.standingOnClimableTile = false
+    for i=1,length do
+        local collision = collisions[i]
+        local collisionType = collision.type
+        if collisionType == gfx.sprite.kCollisionTypeOverlap then
+            local collisionTag = collision.other:getTag()
+            if collisionTag == TAGS.CLIMABLE then
+                self.touchingClimableTile = true
+                self.climbTileX = collision.other.x
+                if collision.normal.y == -1 and not collision.overlaps then
+                    self.standingOnClimableTile = true
+                    self.touchedGround = true
+                    self:moveTo(self.x, originalY)
+                end
+            end
+        else
+            if collision.normal.y == -1 then
+                self.touchedGround = true
+            elseif collision.normal.y == 1 then
+                self.touchedCeiling = true
+            end
+            if collision.normal.x == -1 or collision.normal.x == 1 then
+                self.touchedWall = true
+            end
+        end
+    end
+
+    if self.touchedGround or self.touchedCeiling then
+        self.yVelocity = 0
+    end
+
+    if self.touchedWall then
+        self.xVelocity = 0
+    end
+end
+
 function Player:resetPlayer()
     self:moveTo(self.respawnX, self.respawnY)
     self.xVelocity = 0
     self.yVelocity = 0
 end
 
-function Player:changeToClimbState(collision)
-    local climableTileX = collision.other.x
-    if math.abs(self.x - climableTileX) > self.climbMagnetRange then
+function Player:checkIfClimbing()
+    if self.touchingClimableTile then
+        if self.standingOnClimableTile then
+            if pd.buttonIsPressed(pd.kButtonDown) then
+                self:changeToClimbState()
+            end
+        elseif pd.buttonIsPressed(pd.kButtonUp) then
+            self:changeToClimbState()
+        end
+    end
+end
+
+function Player:changeToClimbState()
+    if math.abs(self.x - self.climbTileX) > self.climbMagnetRange then
         return
     end
     self.xVelocity = 0
     self.yVelocity = 0
     self:changeState("climb")
     self:pauseAnimation()
-    self:moveTo(climableTileX, self.y)
+    self:moveTo(self.climbTileX, self.y + 1)
+end
+
+function Player:changeToJumpState()
+    self.yVelocity = self.jumpVelocity
+    self:changeState("jumpAscent")
+end
+
+function Player:changeToRunState(direction)
+    if direction == "left" then
+        self.xVelocity = -self.startVelocity
+            self.globalFlip = 1
+    elseif direction == "right" then
+        self.xVelocity = self.startVelocity
+        self.globalFlip = 0
+    end
+    self:changeState("run")
 end
 
 function Player:handleJumpPhysics()
@@ -230,7 +260,9 @@ function Player:accelerateRight()
 end
 
 function Player:applyGravity()
-    if self.yVelocity < 0 then
+    if self.standingOnClimableTile then
+        self.yVelocity = 0
+    elseif self.yVelocity < 0 then
         self.yVelocity += self.gravity
     else
         self.yVelocity += self.fallingGravity
